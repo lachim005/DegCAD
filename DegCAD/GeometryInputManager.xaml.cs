1c442f4c06b25414696fb1c77a10b1df49368f11
lachim005 <lachim005@seznam.cz>
2022-10-25 20:47:05 +0200

    Adds GetPoint to the input manager

--- DegCAD/GeometryInputManager.xaml.cs
+++ DegCAD/GeometryInputManager.xaml.cs
@@ -32,5 +32,53 @@ namespace DegCAD
             ViewPort = viewPort;
             PreviewVp = previewVp;
         }
+
+        /// <summary>
+        /// Gets a point from the user
+        /// </summary>
+        /// <param name="preview">Action that will get executed to redraw the preview of the inputed point</param>
+        public async Task<Vector2> GetPoint(Action<Vector2, GeometryDrawer> preview)
+        {
+            //Saves the previewPoint handler so it can be unasigned later
+            EventHandler<VPMouseEventArgs> previewPoint = (s, e) =>
+            {
+                PreviewGd.Clear();
+                preview(e.CanvasPos, PreviewGd);
+            };
+
+            //Redraws the preview when the user moves the mouse or zooms/pans the viewport
+            ViewPort.VPMouseMoved += previewPoint;
+            ViewPort.ViewportChanged += previewPoint;
+
+            //Used to await the click
+            TaskCompletionSource<Vector2> result = new();
+
+            //Saves the handler so it can be unasigned
+            MouseButtonEventHandler viewPortClick = (s, e) =>
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    Vector2 res = Mouse.GetPosition(ViewPort);
+                    result.SetResult(res);
+                }
+            };
+
+            ViewPort.MouseDown += viewPortClick;
+
+            //Draws the preview so it doesn't appear after the user moves their mouse
+            preview(PreviewVp.ScreenToCanvas(Mouse.GetPosition(ViewPort)), PreviewGd);
+
+            //Awaits the user click
+            Vector2 mposClick = ViewPort.ScreenToCanvas(await result.Task);
+
+            //Unasignes all events and clears the preview
+            ViewPort.VPMouseMoved -= previewPoint;
+            ViewPort.MouseDown -= viewPortClick;
+            ViewPort.ViewportChanged -= previewPoint;
+            PreviewGd.Clear();
+
+
+            return mposClick;
+        }
     }
 }
