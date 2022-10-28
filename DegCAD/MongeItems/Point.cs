84def643d1faad1bc5bf04b44d866609de5cd739
lachim005 <lachim005@seznam.cz>
2022-10-28 21:51:28 +0200

    Fixes Monge point

--- DegCAD/MongeItems/Point.cs
+++ DegCAD/MongeItems/Point.cs
@@ -10,16 +10,16 @@
         public void Draw(GeometryDrawer gd)
         {
             //Y point cross
-            if (double.IsNaN(Y))
+            if (!double.IsNaN(Y))
             {
                 gd.DrawLine((X - .2, Y), (X + .2, Y), Style);
                 gd.DrawLine((X, Y - .2), (X, Y + .2), Style);
             }
             //Z point cross
-            if (double.IsNaN(Z))
+            if (!double.IsNaN(Z))
             {
-                gd.DrawLine((X - .2, Z), (X + .2, Z), Style);
-                gd.DrawLine((X, Z - .2), (X, Z + .2), Style);
+                gd.DrawLine((X - .2, -Z), (X + .2, -Z), Style);
+                gd.DrawLine((X, -Z - .2), (X, -Z + .2), Style);
             }
         }
     }
