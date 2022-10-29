28597031921b1062f9177c961f132bf04b88856f
lachim005 <lachim005@seznam.cz>
2022-10-29 22:05:34 +0200

    Makes the snapper snap to points

--- DegCAD/Snapper.cs
+++ DegCAD/Snapper.cs
@@ -8,6 +8,8 @@ namespace DegCAD
 {
     public class Snapper
     {
+        private const double SnapThreshold = .02;
+
         public Timeline Timeline { get; init; }
 
         public Snapper(Timeline timeline)
@@ -17,6 +19,33 @@ namespace DegCAD
 
         public Vector2 Snap(Vector2 v)
         {
+            #region Snap to points
+            Vector2? closestPoint = null;
+            double closestPointDistance = double.MaxValue;
+
+            //Goes through every snapable point and saves the closest one
+            foreach (var cmd in Timeline.CommandHistory)
+            {
+                for (int i = 0; i < cmd.SnapPoints.Length; i++)
+                {
+                    double distance = (cmd.SnapPoints[i] - v).LengthSquared;
+                    if (distance < closestPointDistance)
+                    {
+                        closestPointDistance = distance;
+                        closestPoint = cmd.SnapPoints[i];
+                    }
+                }
+            }
+
+            
+            if (closestPoint is null)
+                return v;
+            //If the closest point is within a threshold, returns the snapped point
+            if (closestPointDistance < SnapThreshold)
+                return (Vector2)closestPoint;
+
+
+            #endregion
             return v;
         }
     }
