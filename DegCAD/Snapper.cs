ca58eafa661c54a970c3195436b453325b5adeb3
lachim005 <lachim005@seznam.cz>
2022-11-13 19:57:36 +0100

    Updates the snapper to snap to snapable lines

--- DegCAD/Snapper.cs
+++ DegCAD/Snapper.cs
@@ -19,7 +19,6 @@ namespace DegCAD
 
         public Vector2 Snap(Vector2 v)
         {
-            #region Snap to points
             Vector2? closestPoint = null;
             double closestPointDistance = double.MaxValue;
 
@@ -40,16 +39,76 @@ namespace DegCAD
                 }
             }
 
-            
-            if (closestPoint is null)
-                return v;
+            //Goes through every snapable line and saves all the ones that are close
+            List<ParametricLine2> closeLines = new();
+
+            foreach(var cmd in Timeline.CommandHistory)
+            {
+                for (int i = 0; i < cmd.Items.Length; i++)
+                {
+                    for (int j = 0; j < cmd.Items[i].SnapableLines.Length; j++)
+                    {
+                        var line = cmd.Items[i].SnapableLines[j];
+                        var point = TranslatePointToLine(line, v);
+                        double distance = (v - point).LengthSquared;
+                        if (distance < SnapThreshold)
+                        {
+                            closeLines.Add(line);
+                        }
+                    }
+                }
+            }
+
+            //If there are more close lines, calculates all the intersections
+            if (closeLines.Count > 1)
+            {
+                List<Vector2> intersectionPts = new();
+
+                //calculates all the intersections
+                for (int i = 0; i < closeLines.Count; i++)
+                {
+                    for (int j = 0; j < closeLines.Count; j++)
+                    {
+                        if (i == j) continue;
+                        intersectionPts.Add(closeLines[i].FindIntersection(closeLines[j]));
+                    }
+                }
+
+                //Saves the closest intersection point if it is closer than any other snapable point
+                for (int i = 0; i < intersectionPts.Count; i++)
+                {
+                    double distance = (v - intersectionPts[i]).LengthSquared;
+                    if (distance < closestPointDistance)
+                    {
+                        closestPoint = intersectionPts[i];
+                        closestPointDistance = distance;
+                    }
+                }
+            }
+
             //If the closest point is within a threshold, returns the snapped point
-            if (closestPointDistance < SnapThreshold)
+            if (closestPoint is not null && closestPointDistance < SnapThreshold)
                 return (Vector2)closestPoint;
 
-
-            #endregion
+            //Else returns the point snapped to a line
+            if (closeLines.Count > 0)
+            {
+                return TranslatePointToLine(closeLines[0], v);
+            }
+            
+            //Else returns the original point
             return v;
+        }
+
+        /// <summary>
+        /// Calculates a point on a line that is closest to the input point
+        /// </summary>
+        private Vector2 TranslatePointToLine(ParametricLine2 line, Vector2 pt)
+        {
+            //Creates a perpendicular line passing through the set point
+            var perpLine = new ParametricLine2(pt, (line.DirectionVector.Y, -line.DirectionVector.X));
+            //Returns the intersection point of the two lines
+            return line.FindIntersection(perpLine);
         }
     }
 }
