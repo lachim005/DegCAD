6741ef8819290e2fc9b1c33b37567728e92db2f2
lachim005 <97552235+lachim005@users.noreply.github.com>
2022-12-30 18:45:55 +0100

    Adds equal operators

--- DegCAD/Vector2.cs
+++ DegCAD/Vector2.cs
@@ -58,6 +58,8 @@ namespace DegCAD
 
         public static Vector2 operator *(Vector2 v, double d) => (v.X * d, v.Y * d);
         public static Vector2 operator /(Vector2 v, double d) => (v.X / d, v.Y / d);
+        public static bool operator ==(Vector2 v1, Vector2 v2) => v1.X == v2.X && v1.Y == v2.Y;
+        public static bool operator !=(Vector2 v1, Vector2 v2) => v1.X != v2.X || v1.Y != v2.Y;
 
         public static Vector2 operator -(Vector2 v)
         {
