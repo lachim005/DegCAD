616bd10d7365796ed912a9b2cd288c7302018162
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-01-02 18:08:29 +0100

    Adds a Circle monge item

---
+++ DegCAD/MongeItems/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegCAD.MongeItems
+{
+    /// <summary>
+    /// Circle defined by it's center and a point on it
+    /// </summary>
+    public class Circle : IMongeItem
+    {
+        public Vector2[] SnapablePoints { get; init; }
+        public ParametricLine2[] SnapableLines { get; init; }
+        public Vector2 Center { get; init; }
+        public Vector2 PointOnCircle { get; init; }
+        public Style Style { get; init; }
+
+        public Circle(Vector2 center, Vector2 pointOnCircle, Style style)
+        {
+            PointOnCircle = pointOnCircle;
+            Center = center;
+            Style = style;
+
+            SnapableLines = new ParametricLine2[0];
+            SnapablePoints = new Vector2[1] { center };
+        }
+
+        public void Draw(GeometryDrawer gd)
+        {
+            gd.DrawCircle(Center, PointOnCircle, Style);
+        }
+    }
+}
