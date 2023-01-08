78c21b7c34eeea01df6621a849e0ed8bb0fe9987
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-01-08 15:20:58 +0100

    Adds a Label monge item

---
+++ DegCAD/MongeItems/Label.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegCAD.MongeItems
+{
+    /// <summary>
+    /// A label labeling a thing that's supposed to be labeled
+    /// </summary>
+    public class Label : IMongeItem
+    {
+        public string LabelText { get; set; }
+        public string Subscript { get; set; }
+        public string Superscript { get; set; }
+
+        public Vector2 Position { get; set; }
+        public Vector2 Size { get; private set; }
+
+        public Vector2[] SnapablePoints { get; } = new Vector2[0];
+
+        public ParametricLine2[] SnapableLines { get; } = new ParametricLine2[0];
+
+        public Circle2[] SnapableCircles { get; } = new Circle2[0];
+
+        public Style Style { get; init; }
+
+        public Label(string labelText, string subscript, string superscript, Vector2 position, Style style)
+        {
+            LabelText = labelText;
+            Subscript = subscript;
+            Superscript = superscript;
+            Position = position;
+            Style = style;
+        }
+
+        public void Draw(GeometryDrawer gd)
+        {
+            var endPos = gd.DrawString(LabelText, Position, 16, Style);
+            Size = endPos - Position;
+            endPos.X -= .1;
+            gd.DrawString(Superscript, (endPos.X, Position.Y), 8, Style);
+            gd.DrawString(Subscript, (endPos.X, Position.Y + .3), 8, Style);
+        }
+    }
+}
