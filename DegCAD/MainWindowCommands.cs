1f9f6c614772227c6ba037f4def19d981f44b422
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-01-19 17:02:02 +0100

    Adds a menu and defines some commands

---
+++ DegCAD/MainWindowCommands.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DegCAD
+{
+    public partial class MainWindow
+    {
+        private void CanExecuteEditorCommand(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            if (ActiveEditor is null)
+            {
+                e.CanExecute = false;
+                return;
+            }
+            if (ActiveEditor.ExecutingCommand)
+            {
+                e.CanExecute = false;
+                return;
+            }
+        }
+
+        private void NewCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+        private void OpenCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+        private void SaveCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+        private void SaveAsCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+        private void UndoCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+        }
+        private void RedoCommand(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+        }
+    }
+}
