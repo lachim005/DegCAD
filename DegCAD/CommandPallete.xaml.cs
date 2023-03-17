81581a41f3f183a5458d801f7c3fef48d9972fcb
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-03-17 21:52:30 +0100

    Moves the list of commands to the command palette

--- DegCAD/CommandPallete.xaml.cs
+++ DegCAD/CommandPallete.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DegCAD.GeometryCommands;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -9,7 +11,6 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,6 +22,8 @@ namespace DegCAD
     /// </summary>
     public partial class CommandPallete : UserControl
     {
+        MainWindow? mw;
+
         public CommandPallete()
         {
             InitializeComponent();
@@ -31,25 +34,38 @@ namespace DegCAD
         /// </summary>
         public void GenerateCommands(MainWindow mw)
         {
-            List<GeometryCommandData> commands = GeometryCommandsList.GetCommands((c) => mw.ActiveEditor?.ExecuteCommand(c));
+            Action<IGeometryCommand> executeCommand = (c) => mw.ActiveEditor?.ExecuteCommand(c);
+            this.mw = mw;
 
-            foreach (var cmd in commands)
-            {
-                //Creates the RoutedUICommand to work with keyboard shortcuts
-                RoutedCommand rcmd = new RoutedUICommand(
-                    cmd.Name,
-                    cmd.Description,
-                    typeof(MainWindow),
-                    new InputGestureCollection() { new KeyGesture(cmd.Key, cmd.ModifierKey)
-                });
+            AddCommand(new("Průměty bodu", "Umístí do půdorysny a nárysny průměty bodu", Key.B, ModifierKeys.Control, (_, _) => executeCommand(new Point3D())));
+            AddCommand(new("Vynést body", "Umístí body podle zadaných souřadnic", Key.B, ModifierKeys.Control | ModifierKeys.Shift, (_, _) => executeCommand(new PointCoordInput())));
+            AddCommand(new("Průměty přímky", "Umístí do půdorysny a nárysny průměty přímky", Key.P, ModifierKeys.Control, (_, _) => executeCommand(new Line3D())));
+            AddCommand(new("Úsečka", "Úsečka mezi dvěma body", Key.U, ModifierKeys.Control, (_, _) => executeCommand(new LineSegment())));
+            AddCommand(new("Rovnoběžka", "Rovnoběžka na danou přímku", Key.R, ModifierKeys.Control, (_, _) => executeCommand(new ParallelLine())));
+            AddCommand(new("Kolmice", "Kolmice na danou přímku", Key.K, ModifierKeys.Control, (_, _) => executeCommand(new PerpendicularLine())));
+            AddCommand(new("Střed", "Střed mezi dvěma body", Key.S, ModifierKeys.Control, (_, _) => executeCommand(new Middle())));
+            AddCommand(new("Přenést vzdálenost", "Přenese vzdálenost mezi dvěma body", Key.V, ModifierKeys.Control, (_, _) => executeCommand(new TranslateSize())));
+            AddCommand(new("Sklopit bod", "Sklopí bod pomocí jeho druhého průmětu", Key.S, ModifierKeys.Control | ModifierKeys.Shift, (_, _) => executeCommand(new CastPoint())));
+            AddCommand(new("Kružnice", "Kružnice dána středem a bodem na ní", Key.O, ModifierKeys.Control, (_, _) => executeCommand(new Circle())));
+            AddCommand(new("Oblouk", "Oblouk dán středem, bodem na kružnici a dvěma úhly", Key.O, ModifierKeys.Control | ModifierKeys.Shift, (_, _) => executeCommand(new Arc())));
 
-                //Binds the new command to the main window. Can execute if active editor isn't null
-                mw.CommandBindings.Add(new(rcmd, cmd.ExecuteHandler, mw.CanExecuteEditorCommand));
+        }
 
-                //Adds a button for every command to the toolbar
-                cmdButtons.Children.Add(new Button() { Content = cmd.Name, ToolTip = cmd.Description, Command=rcmd });
-            }
+        public void AddCommand(GeometryCommandData cmd)
+        {
+            //Creates the RoutedUICommand to work with keyboard shortcuts
+            RoutedCommand rcmd = new RoutedUICommand(
+                cmd.Name,
+                cmd.Description,
+                typeof(MainWindow),
+                new InputGestureCollection() { new KeyGesture(cmd.Key, cmd.ModifierKey)
+            });
 
+            //Binds the new command to the main window. Can execute if active editor isn't null
+            mw?.CommandBindings.Add(new(rcmd, cmd.ExecuteHandler, mw.CanExecuteEditorCommand));
+
+            //Adds a button for every command to the toolbar
+            cmdButtons.Children.Add(new Button() { Content = cmd.Name, ToolTip = cmd.Description, Command=rcmd });
         }
     }
 }
