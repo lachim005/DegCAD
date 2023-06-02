46aa171871712284e212189476c6636ccdf426b8
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-06-02 11:50:27 +0200

    Adds some more info to the about window

--- DegCAD/Dialogs/AboutDialog.xaml.cs
+++ DegCAD/Dialogs/AboutDialog.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DegCAD.Dialogs
 {
@@ -37,6 +26,16 @@ namespace DegCAD.Dialogs
         private void CloseClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = e.Uri.ToString(),
+                UseShellExecute = true
+            };
+            Process.Start(psi);
         }
     }
 }
