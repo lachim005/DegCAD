fa6b14bb8b981390f427c54fbd4c99f1f08cd976
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-06-10 23:12:04 +0200

    Adds a paper size select to the page layout window

--- DegCAD/Dialogs/PageLayoutWindow.xaml.cs
+++ DegCAD/Dialogs/PageLayoutWindow.xaml.cs
@@ -19,9 +19,63 @@ namespace DegCAD.Dialogs
     /// </summary>
     public partial class PageLayoutWindow : Window
     {
+        Timeline Timeline { get; init; }
+        ViewPort ViewPort { get; init; }
+
         public PageLayoutWindow(Timeline tl)
         {
             InitializeComponent();
+            Timeline = tl.Clone();
+
+            ViewPort = new(Timeline);
+            Timeline.SetViewportLayer(ViewPort.Layers[1]);
+            vpBorder.Child = ViewPort;
+
+            ViewPort.CanZoom = false;
+        }
+
+        private void PaperSizeChanged(object sender, TextChangedEventArgs e) => RecalculateSize();
+        private void WindowSizeChanged(object sender, SizeChangedEventArgs e) => RecalculateSize();
+        private void WindowLoaded(object sender, RoutedEventArgs e) => RecalculateSize();
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!IsLoaded) return;
+            if (paperPresetCustom.IsChecked == true) 
+            {
+                paperWidth.IsEnabled = true;
+                paperHeight.IsEnabled = true;
+                return;
+            };
+            paperWidth.IsEnabled = false;
+            paperHeight.IsEnabled = false;
+            if (paperPresetA3.IsChecked == true)
+            {
+                paperWidth.Text = "297";
+                paperHeight.Text = "420";
+            }
+            if (paperPresetA4.IsChecked == true)
+            {
+                paperWidth.Text = "210";
+                paperHeight.Text = "297";
+            }
+            if (paperPresetA5.IsChecked == true)
+            {
+                paperWidth.Text = "148";
+                paperHeight.Text = "210";
+            }
+            RecalculateSize();
+        }
+
+        private void RecalculateSize()
+        {
+            if (!IsLoaded) return;
+
+            if (!int.TryParse(paperWidth.Text, out var w)) return;
+            if (!int.TryParse(paperHeight.Text, out var h)) return;
+            double scaleFactor = Math.Min(vpGrid.ActualHeight / h, vpGrid.ActualWidth / w);
+            vpBorder.Height = h * scaleFactor;
+            vpBorder.Width = w * scaleFactor;
+            ViewPort.Scale = 10 * scaleFactor / ViewPort.unitSize;
         }
     }
 }
