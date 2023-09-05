10a754997ee26f56a4f7a1e593620d65353f5d0e
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-09-05 21:07:19 +0200

    Makes step description editable and steps removable

--- DegCAD/Guides/EditorGuideEditorView.xaml.cs
+++ DegCAD/Guides/EditorGuideEditorView.xaml.cs
@@ -23,9 +23,15 @@ namespace DegCAD.Guides
         private Guide guide;
         private ViewPort vp;
         private Timeline clonedTl;
+
+        private GuideStep selectedStep = new();
+
         public EditorGuideEditorView(Timeline tl, Guide g)
         {
+
             InitializeComponent();
+
+            stepEditor.DataContext = selectedStep;
             guide = g;
 
             clonedTl = tl.Clone();
@@ -40,12 +46,39 @@ namespace DegCAD.Guides
 
         private void StepButtonClick(object sender, RoutedEventArgs e)
         {
+            if (sender is not Button senderBtn) return;
+            if (senderBtn.DataContext is not GuideStep step) return;
 
+            for (int i = 0; i < stepsIc.Items.Count; i++)
+            {
+                var cp = (ContentPresenter)stepsIc.ItemContainerGenerator.ContainerFromIndex(i);
+                var it = cp.ContentTemplate.FindName("stepButton", cp);
+                if (it is not Button btn) break;
+                btn.IsEnabled = true;
+            }
+
+            senderBtn.IsEnabled = false;
+            StepSelected(step);
+        }
+
+        private void StepSelected(GuideStep step)
+        {
+            stepEditor.DataContext = step;
+            selectedStep = step;
         }
 
         private void AddStep(object sender, RoutedEventArgs e)
         {
             guide.Steps.Add(new() { Position = guide.Steps.Count + 1 });
+        }
+
+        private void RemoveStep(object sender, RoutedEventArgs e)
+        {
+            guide.Steps.Remove(selectedStep);
+            for (int i = 0; i < guide.Steps.Count; i++)
+            {
+                guide.Steps[i].Position = i + 1;
+            }
         }
     }
 }
