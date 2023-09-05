543487e4af6351465a9d78f718ef3dd882a8297a
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-09-05 19:28:22 +0200

    Moves everything guide-related into it's own folder

--- DegCAD/Guides/EditorGuideEditorView.xaml.cs
+++ DegCAD/Guides/EditorGuideEditorView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DegCAD
+namespace DegCAD.Guides
 {
     /// <summary>
     /// Interaction logic for EditorGuideEditorView.xaml
