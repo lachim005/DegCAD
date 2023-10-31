e5d453a875b88929d1e2adfaaf1499e604bc9a02
lachim005 <97552235+lachim005@users.noreply.github.com>
2023-10-31 22:39:43 +0100

    Adds a unit size text box to print dialog

--- DegCAD/Dialogs/PrintDialog.xaml.cs
+++ DegCAD/Dialogs/PrintDialog.xaml.cs
@@ -87,6 +87,7 @@ namespace DegCAD.Dialogs
         {
             if (selectedQueue is null) return;
             if (capabilities is null) return;
+            if (!double.TryParse(unitSizeTbx.Text, out double unitSize) || unitSize <= .1) return;
             if (GetPaperSize() is not PageMediaSize size) return;
 
             if (!int.TryParse(copyCountTbx.Text, out int copyCount) || copyCount < 1)
@@ -102,7 +103,7 @@ namespace DegCAD.Dialogs
             double h = pt.PageMediaSize.Height.GetValueOrDefault(0);
 
             var clonedVp = vp.Clone();
-            clonedVp.Scale = 10d * 96d / 25.4 / ViewPort.unitSize;
+            clonedVp.Scale = unitSize * 96d / 25.4 / ViewPort.unitSize;
             clonedVp.Width = w;
             clonedVp.Height = h;
             
@@ -144,8 +145,9 @@ namespace DegCAD.Dialogs
 
         private void ResizePaperPreview()
         {
+            if (!IsLoaded) return;
             if (selectedQueue is null) return;
-
+            if (!double.TryParse(unitSizeTbx.Text, out double unitSize) || unitSize <= .1) return;
             if (GetPaperSize() is not PageMediaSize size) return;
 
             PrintTicket pt = selectedQueue.UserPrintTicket;
@@ -159,7 +161,8 @@ namespace DegCAD.Dialogs
             vp.Width = w * scaleFactor;
             vp.Height = h * scaleFactor;
 
-            vp.Scale = 10d * 96d * scaleFactor / 25.4 / ViewPort.unitSize;
+            vp.Scale = unitSize * 96d * scaleFactor / 25.4 / ViewPort.unitSize;
+            vp.Redraw();
         }
 
 
@@ -177,5 +180,14 @@ namespace DegCAD.Dialogs
             return null;
         }
 
+        private void UnitSizeTbxTextChanged(object sender, TextChangedEventArgs e)
+        {      
+            ResizePaperPreview();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            ResizePaperPreview();
+        }
     }
 }
