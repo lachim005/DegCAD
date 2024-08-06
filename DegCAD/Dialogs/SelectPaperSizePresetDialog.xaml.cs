91b7d6bf1606912ec7db511925ea9d1fb87d3272
lachim005 <97552235+lachim005@users.noreply.github.com>
2024-08-06 23:38:58 +0200

    Adds a dialog for selecting a paper size preset

---
+++ DegCAD/Dialogs/SelectPaperSizePresetDialog.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DegCAD.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for SelectPaperSizePresetDialog.xaml
+    /// </summary>
+    public partial class SelectPaperSizePresetDialog : Window
+    {
+        public static bool lastLandscape = false;
+
+        public PaperSizePreset? SelectedPreset { get; set; }
+        public ObservableCollection<PaperSizePreset> Presets { get; set; }
+
+        public SelectPaperSizePresetDialog()
+        {
+            InitializeComponent();
+            Presets = new(Settings.PaperSizePresets);
+            paperPresetsIC.ItemsSource = Presets;
+
+            landscapeCbx.IsChecked = lastLandscape;
+            if (lastLandscape)
+            {
+                SwapSizes();
+            }
+        }
+
+        public static PaperSizePreset? GetPaperSizePreset()
+        {
+            SelectPaperSizePresetDialog dialog = new();
+            dialog.ShowDialog();
+            return dialog.SelectedPreset;
+        }
+
+        private void PresetClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is not FrameworkElement fe) return;
+            if (fe.DataContext is not PaperSizePreset preset) return;
+            SelectedPreset = preset;
+            Close();
+        }
+
+        private void OnWindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            lastLandscape = landscapeCbx.IsChecked == true;
+        }
+
+        private void LandscapeChanged(object sender, RoutedEventArgs e)
+        {
+            SwapSizes();
+        }
+
+        private void SwapSizes()
+        {
+            foreach (var item in Presets)
+            {
+                var tmp = item.Height;
+                item.Height = item.Width;
+                item.Width = tmp;
+            }
+        }
+    }
+}
--- DegCAD/PaperSizePreset.cs
+++ DegCAD/PaperSizePreset.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,20 +8,45 @@ using System.Windows;
 
 namespace DegCAD
 {
-    public class PaperSizePreset
+    public class PaperSizePreset : INotifyPropertyChanged
     {
+        private double _width;
+        private double _height;
+
         public string Name { get; set; }
-        public double Width { get; set; }
-        public double Height { get; set; }
-        public Visibility ShowFold 
-        { 
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+                PropertyChanged?.Invoke(this, new(nameof(Width)));
+            }
+        }
+        public double Height
+        {
+            get
+            { 
+                return _height;
+            }
+            set
+            {
+                _height = value;
+                PropertyChanged?.Invoke(this, new(nameof(Height)));
+            }
+        }
+        public Visibility ShowFold
+        {
             get
             {
                 double ratio = Width / Height;
                 if (ratio < 1)
                     ratio = 1 / ratio;
                 return (ratio < 5) ? Visibility.Visible : Visibility.Collapsed;
-            } 
+            }
         }
 
         public PaperSizePreset(string name, double width, double height)
@@ -29,5 +55,7 @@ namespace DegCAD
             Width = width;
             Height = height;
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
