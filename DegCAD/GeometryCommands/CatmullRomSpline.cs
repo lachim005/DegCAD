c03175b03994a708c8af37738f8f5ffc24b42849
lachim005 <97552235+lachim005@users.noreply.github.com>
2024-08-19 16:46:52 +0200

    Adds a Catmull-Rom spline geometry element and command

---
+++ DegCAD/GeometryCommands/CatmullRomSpline.cs
@@ -0,0 +1,136 @@
+﻿using DegCAD.TimelineElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegCAD.GeometryCommands
+{
+    public class CatmullRomSpline : IGeometryCommand
+    {
+        public async Task<TimelineItem?> ExecuteAsync(ViewportLayer previewVpl, ViewportLayer vpl, ViewportLayer bgVpl, GeometryInputManager inputMgr, EditorStatusBar esb)
+        {
+            esb.CommandName = "Křivka body";
+
+            esb.CommandHelp = "Vyberte bod údávající směr prvního konce křivky";
+
+            
+
+            List<Point> points = [new(0, 0, previewVpl)];
+
+            List<Vector2> splinePoints = [];
+
+            Vector2 p1 = await inputMgr.GetPoint((pt) =>
+            {
+                points[^1].Coords = pt;
+                foreach (Point p in points)
+                {
+                    p.Draw();
+                }
+            });
+            splinePoints.Add(p1);
+            points.Add(new(0, 0, previewVpl));
+
+
+            TimelineElements.LineSegment startSeg = new(p1, (0, 0), Style.BlueDashStyle, previewVpl);
+            esb.CommandHelp = "Vyberte první bod křivky";
+
+            Vector2 p2 = await inputMgr.GetPoint((pt) =>
+            {
+                points[^1].Coords = pt;
+                startSeg.P2 = pt;
+                startSeg.Draw();
+                foreach (Point p in points)
+                {
+                    p.Draw();
+                }
+            });
+            points.Add(new(0, 0, previewVpl));
+
+            TimelineElements.LineSegment endSeg = new(p2, (0, 0), Style.BlueDashStyle, previewVpl);
+            esb.CommandHelp = "Vyberte druhý bod křivky";
+            splinePoints.Add(p2);
+            Vector2 p3 = await inputMgr.GetPoint((pt) =>
+            {
+                points[^1].Coords = pt;
+                startSeg.Draw();
+                endSeg.P2 = pt;
+                endSeg.Draw();
+                foreach (Point p in points)
+                {
+                    p.Draw();
+                }
+            });
+            splinePoints.Add(p3);
+
+            esb.CommandHelp = "Vyberte třetí bod křivky";
+
+            CRSpline newSplineSegment = new(Style.GreenStyle, previewVpl, p1, p2, p3, (0, 0));
+            endSeg.P1 = p3;
+
+            Vector2 p4 = await inputMgr.GetPoint((pt) =>
+            {
+                points[^1].Coords = pt;
+                startSeg.Draw();
+                endSeg.P2 = pt;
+                endSeg.Draw();
+
+                newSplineSegment.Points[^1] = pt;
+                newSplineSegment.CalculatePoints();
+                newSplineSegment.Draw();
+
+                foreach (Point p in points)
+                {
+                    p.Draw();
+                }
+            });
+            splinePoints.Add(p4);
+
+            esb.CommandHelp = "Vyberte další body, jakmile budete mít všechny body, zmáčkněte ESC nebo tlačítko pro zrušení příkazu";
+
+            CRSpline resultSpline = new(Style.HighlightStyle, previewVpl, p1, p2, p3, p4);
+
+            try
+            {
+                while (true)
+                {
+                    points.Add(new(0, 0, previewVpl));
+                    endSeg.P1 = splinePoints[^1];
+
+                    newSplineSegment.Points.RemoveAt(0);
+                    newSplineSegment.Points.Add((0,0));
+
+                    Vector2 pt = await inputMgr.GetPoint((pt) =>
+                    {
+                        points[^1].Coords = pt;
+                        newSplineSegment.Points[^1] = pt;
+                        newSplineSegment.CalculatePoints();
+                        newSplineSegment.Draw();
+                        resultSpline.Draw();
+
+                        startSeg.Draw();
+                        endSeg.P2 = pt;
+                        endSeg.Draw();
+
+                        foreach (Point p in points)
+                        {
+                            p.Draw();
+                        }
+                    });
+                    splinePoints.Add(pt);
+                    resultSpline.Points.Add(pt);
+                    resultSpline.CalculatePoints();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is not CommandCanceledException) throw;
+            }
+
+          
+
+            return new TimelineItem([new CRSpline(inputMgr.StyleSelector.CurrentStyle, vpl, [.. splinePoints])]);
+        }
+    }
+}
---
+++ DegCAD/TimelineElements/CRSpline.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DegCAD.TimelineElements
+{
+    public class CRSpline : GeometryElement
+    {
+        private readonly Path _spline;
+
+        public List<Vector2> Points;
+        public List<Vector2> ControlPoints = [];
+
+        public CRSpline(Style s, ViewportLayer? vpl, params Vector2[] points)
+        {
+            if (points.Length < 4)
+            {
+                throw new ArgumentException("Příliš malý počet bodů");
+            }
+
+            Points = new(points);
+            CalculatePoints();
+
+            _spline = new();
+            AddShape(_spline);
+
+            Style = s;
+
+            if (vpl is not null)
+            {
+                AddToViewportLayer(vpl);
+            }
+        }
+
+        public void CalculatePoints()
+        {
+            ControlPoints.Clear();
+
+            for (int i = 1; i < Points.Count - 1; i++)
+            {
+                ControlPoints.Add(Points[i] + ((Points[i - 1] - Points[i + 1]) / 6));
+                ControlPoints.Add(Points[i] - ((Points[i - 1] - Points[i + 1]) / 6));
+            }
+        }
+
+        public override GeometryElement CloneElement()
+        {
+            return new CRSpline(Style, null, [.. Points]);
+        }
+
+        public override void Draw()
+        {
+            if (ViewportLayer is null) return;
+
+            var vp = ViewportLayer.Viewport;
+
+            StringBuilder sb = new();
+            var startingPoint = vp.CanvasToScreen(Points[1]);
+            sb.Append("M " + startingPoint.X.ToString(CultureInfo.InvariantCulture) + " " + startingPoint.Y.ToString(CultureInfo.InvariantCulture));
+
+            int cpc = 1;
+            for (int i = 1; i < Points.Count - 2; i++)
+            {
+                sb.Append(" C ");
+                var cp1 = vp.CanvasToScreen(ControlPoints[cpc++]);
+                sb.Append(cp1.X.ToString(CultureInfo.InvariantCulture) + " " + cp1.Y.ToString(CultureInfo.InvariantCulture));
+                sb.Append(", ");
+                var cp2 = vp.CanvasToScreen(ControlPoints[cpc++]);
+                sb.Append(cp2.X.ToString(CultureInfo.InvariantCulture) + " " + cp2.Y.ToString(CultureInfo.InvariantCulture));
+                sb.Append(", ");
+                var pt = vp.CanvasToScreen(Points[i + 1]);
+                sb.Append(pt.X.ToString(CultureInfo.InvariantCulture) + " " + pt.Y.ToString(CultureInfo.InvariantCulture));
+            }
+
+            _spline.Data = Geometry.Parse(sb.ToString());
+        }
+    }
+}
